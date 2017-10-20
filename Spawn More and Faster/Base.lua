8c4f33df4ae7482e50acfedb29b91c5e6f36f172
Dr_Newbie <tf2baidonation@gmail.com>
2017-10-20 13:45:48 +0800

    28 ; Use GitHub Update

--- Spawn More and Faster/Base.lua
+++ Spawn More and Faster/Base.lua
@@ -6,8 +6,9 @@ if UpdateThisMod then
 	UpdateThisMod:Add({
 		mod_id = 'Spawn More and Faster',
 		data = {
-			modworkshop_id = 20649
+			modworkshop_id = 20649,
+            dl_url = 'https://github.com/DrNewbie/BigParty/raw/master/Spawn%20More%20and%20Faster.zip',
+            info_url = 'https://raw.githubusercontent.com/DrNewbie/BigParty/master/Spawn%20More%20and%20Faster/mod.txt'
 		}
 	})
-	UpdateThisMod:Loop()
 end
