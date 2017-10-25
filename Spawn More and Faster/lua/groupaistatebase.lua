032d1da2223042179186971345ff846be1794918
Dr_Newbie <tf2baidonation@gmail.com>
2017-10-25 19:15:23 +0800

    33

--- Spawn More and Faster/lua/groupaistatebase.lua
+++ Spawn More and Faster/lua/groupaistatebase.lua
@@ -2,6 +2,10 @@ function GroupAIStateBase:_get_difficulty_dependent_value(tweak_values)
 	return math.lerp(tweak_values[self._difficulty_point_index], tweak_values[self._difficulty_point_index + 1], self._difficulty_ramp)
 end
 
+function GroupAIStateBase:_get_balancing_multiplier(balance_multipliers)
+	return balance_multipliers[#balance_multipliers]
+end
+
 function GroupAIStateBase:set_difficulty()
 	self._difficulty_value = 1
 	self:_calculate_difficulty_ratio()
