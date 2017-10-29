801ce2967b0aa634c0ac6037922f4438708136c4
Dr_Newbie <tf2baidonation@gmail.com>
2017-10-30 01:35:34 +0800

    back2old

--- Spawn More and Faster/Base.lua
+++ Spawn More and Faster/Base.lua
@@ -6,9 +6,9 @@ if UpdateThisMod then
 	UpdateThisMod:Add({
 		mod_id = 'Spawn More and Faster',
 		data = {
-			modworkshop_id = "BigParty_20649",
-			dl_url = 'https://drnewbie.github.io/BigParty/Spawn%20More%20and%20Faster.zip',
-			info_url = 'https://drnewbie.github.io/BigParty/Spawn%20More%20and%20Faster/mod.txt'
+			modworkshop_id = 20649,
+            dl_url = 'https://drnewbie.github.io/BigParty/Spawn%20More%20and%20Faster.zip',
+            info_url = 'https://drnewbie.github.io/BigParty/Spawn%20More%20and%20Faster/mod.txt'
 		}
 	})
 end
