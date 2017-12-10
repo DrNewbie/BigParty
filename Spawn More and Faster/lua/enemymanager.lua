17fcccc4c7f2279e5f348ab9adc6daf4ea5d03f6
Dr_Newbie <tf2baidonation@gmail.com>
2017-12-10 18:30:29 +0800

    46 ; some cops shouldn't be cap

--- Spawn More and Faster/lua/enemymanager.lua
+++ Spawn More and Faster/lua/enemymanager.lua
@@ -4,6 +4,15 @@ Hooks:PostHook(EnemyManager, "init", "SMF_EnemyManager_init", function(self)
 	self._sfm_delay = 0
 end)
 
+local LogicNotAllow = {
+	inactive = true,
+	intimidated = true,
+	arrest = true,
+	sniper = true,
+	trade = true,
+	phalanx = true
+}
+
 Hooks:PostHook(EnemyManager, "update", "SMF_EnemyManager_update", function(self, t)
 	if MoreEnemies.Settings.force_cap and t > self._sfm_delay then
 		self._sfm_delay = t + 10
@@ -15,19 +24,23 @@ Hooks:PostHook(EnemyManager, "update", "SMF_EnemyManager_update", function(self,
 				local cat_count = {}
 				local sp_limit = tweak_data.group_ai.special_unit_spawn_limits
 				for u_key, u_data in pairs(self._enemy_data.unit_data) do
-					if not u_data or not u_data.unit or not alive(u_data.unit) or not u_data.unit:character_damage() or not u_data.unit:character_damage().damage_mission or u_data.unit:character_damage()._invulnerable or u_data.unit:character_damage()._immortal or u_data.unit:character_damage()._dead or managers.groupai:state():is_enemy_converted_to_criminal(u_data.unit) or not u_data.unit:base()._tweak_table then
+					if not u_data or not u_data.unit or not alive(u_data.unit) or not u_data.unit:character_damage() or not u_data.unit:character_damage():can_kill() or u_data.unit:character_damage()._immortal or u_data.unit:character_damage():dead() or managers.groupai:state():is_enemy_converted_to_criminal(u_data.unit) or not u_data.unit:base()._tweak_table then
 					
 					else
-						local cat_name = u_data.unit:base()._tweak_table
-						size = size + 1
-						unit_list[u_key] = u_data.unit
-						if type(sp_limit[cat_name]) == "number" and sp_limit[cat_name] > 0 then
+						if type(u_data.unit:brain()._current_logic_name) ~= "string" or LogicNotAllow[u_data.unit:brain()._current_logic_name] then
 						
 						else
-							cat_name = "Normal"
+							local cat_name = u_data.unit:base()._tweak_table
+							size = size + 1
+							unit_list[u_key] = u_data.unit
+							if type(sp_limit[cat_name]) == "number" and sp_limit[cat_name] > 0 then
+							
+							else
+								cat_name = "Normal"
+							end
+							cat_count[cat_name] = cat_count[cat_name] or 0
+							cat_count[cat_name] = cat_count[cat_name] + 1
 						end
-						cat_count[cat_name] = cat_count[cat_name] or 0
-						cat_count[cat_name] = cat_count[cat_name] + 1
 					end		
 				end
 				if size > MoreEnemies.Settings.force_cap_attach then
