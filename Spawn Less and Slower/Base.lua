4109ca922d2bb8e17f756ffe71be5226f668cbf8
Dr_Newbie <tf2baidonation@gmail.com>
2020-11-29 14:43:45 +0800

    SpawnLessEnemies[7]

--- Spawn Less and Slower/Base.lua
+++ Spawn Less and Slower/Base.lua
@@ -1,9 +1,8 @@
-local _r = 0.49
-local _mul = 0.88
-local _cd_mul = 6.66
-local _clamp = {1, 12}
-
 function set_group_ai_tweak_data()
+	local _r = 0.49
+	local _mul = 0.88
+	local _cd_mul = 6.66
+	local _clamp = {2, 12}
 	if MutatorBigParty and managers.mutators:is_mutator_active(MutatorBigParty) then
 		return
 	end
@@ -11,7 +10,7 @@ function set_group_ai_tweak_data()
 		local group_ai = tweak_data.group_ai
 		local besiege = group_ai.besiege
 		local assault = besiege.assault
-		
+
 		group_ai.special_unit_spawn_limits.tank = math.round(math.clamp(group_ai.special_unit_spawn_limits.tank*_r, 1, 2))
 		group_ai.special_unit_spawn_limits.taser = math.round(math.clamp(group_ai.special_unit_spawn_limits.taser*_r, 1, 3))
 		group_ai.special_unit_spawn_limits.spooc = math.round(math.clamp(group_ai.special_unit_spawn_limits.spooc*_r, 1, 2))
@@ -19,32 +18,19 @@ function set_group_ai_tweak_data()
 		group_ai.special_unit_spawn_limits.medic = math.round(math.clamp(group_ai.special_unit_spawn_limits.medic*_r, 1, 2))
 
 		for id, group in pairs(group_ai.enemy_spawn_groups) do
-			if id ~= "Phalanx" then
-				local is_spooc = id:find("spooc")
-				if group.amount then
-					for k, v in pairs(group.amount) do
-						group.amount[k] = math.round(v * _r) + 1
-						group.amount[k] = math.clamp(group.amount[k], _clamp[1], _clamp[2])
-						if is_spooc then
-							group.amount[k] = math.clamp(group.amount[k], _clamp[1], _clamp[2])
-						end
-					end
-				end
-				for _, spawn in pairs(group.spawn or {}) do
+			for _, spawn in pairs(group.spawn or {}) do
+				if not group_ai.unit_categories[spawn.unit].special_type then
 					if spawn.amount_max then
 						spawn.amount_max = spawn.amount_max * (_r * ((spawn.unit:find("swat") or spawn.unit:find("heavy")) and 1 or _r))
 						spawn.amount_max = math.round(spawn.amount_max) + 1
 						spawn.amount_max = math.clamp(spawn.amount_max, _clamp[1], _clamp[2])
-						if is_spooc then
-							spawn.amount_max = math.clamp(spawn.amount_max, _clamp[1], _clamp[2])
-						end
 					end
 				end
 			end
 		end
 		
 		for i = 1, #assault.force do
-			assault.force[i] = 7 + i
+			assault.force[i] = 8
 		end
 		
 		for i = 1, #assault.force_pool do
@@ -56,11 +42,11 @@ function set_group_ai_tweak_data()
 		end
 		
 		for i = 1, #assault.force_balance_mul do
-			assault.force_balance_mul[i] = 0.5 + i * 0.15
+			assault.force_balance_mul[i] = 1
 		end
 		
 		for i = 1, #assault.force_pool_balance_mul do
-			assault.force_pool_balance_mul[i] = 0.5 + i * 0.85
+			assault.force_pool_balance_mul[i] = 1
 		end
 		
 		for i = 1, #assault.delay do
@@ -106,21 +92,21 @@ if GameSetup then
 end
 
 if GroupAIStateBase then
-	GroupAIStateBase._MAX_SIMULTANEOUS_SPAWNS = 8
+	GroupAIStateBase._MAX_SIMULTANEOUS_SPAWNS = 12
 	function GroupAIStateBase:_get_balancing_multiplier(balance_multipliers)
 		return balance_multipliers[#balance_multipliers]
 	end
 end
 
 if GroupAIStateBesiege then
-	GroupAIStateBesiege._MAX_SIMULTANEOUS_SPAWNS = 8
+	GroupAIStateBesiege._MAX_SIMULTANEOUS_SPAWNS = 12
 	function GroupAIStateBesiege:_get_balancing_multiplier(balance_multipliers)
 		return balance_multipliers[#balance_multipliers]
 	end
 end
 
 if GroupAIStateStreet then
-	GroupAIStateStreet._MAX_SIMULTANEOUS_SPAWNS = 8
+	GroupAIStateStreet._MAX_SIMULTANEOUS_SPAWNS = 12
 	function GroupAIStateStreet:_get_balancing_multiplier(balance_multipliers)
 		return balance_multipliers[#balance_multipliers]
 	end
